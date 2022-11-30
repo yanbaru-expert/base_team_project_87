570a478c8dd11499833966a01f1f5c1b08a14685
oguri-amedo <mungcal.d.lewis@gmail.com>
2022-11-30 23:22:32 +0900

    一覧表示機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 end
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
